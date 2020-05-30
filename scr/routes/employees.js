16a5b02d33332242421b01adf4a50f3e3cbfa1f5
Jhonnier Benavides <63086134+JhonierBenavides@users.noreply.github.com>
2020-05-29 19:19:50 -0500

    EmployeesJB
    Arreglo

--- scr/routes/employees.js
+++ scr/routes/employees.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const mysqlConnection  = require('../database.js');
+const mysqlConnection  = require('../database.js'); 
 
 
 module.exports = router;
